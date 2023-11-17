b77d833fd6d5a540d1afc0c484e404ede898f6be
Péter Földházi Jr <10651180+foldhpet@users.noreply.github.com>
2023-11-16 23:00:11 -0600

    Added first page object

---
+++ business-logic/page-models/home-page.ts
@@ -0,0 +1,15 @@
+import {type Locator, type Page} from '@playwright/test';
+
+export class HomePage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async open() {
+        await this.page.goto('https://www.peterfoldhazi.com/');
+    }
+
+
+}
